c796c081941b277d649c094bf765d8e7eed08577
spacecheng <42579756+spacecheng@users.noreply.github.com>
2024-08-27 10:58:37 -0500

    Create main.py

---
+++ game/main.py
@@ -0,0 +1,17 @@
+char_name =  input("what is your characters name")
+gender = input("what is your gender: 1 = male, 2 = female")
+Char_class = input("what is yout class:1 = cyclopse, 2 = harpy, 3 = human")
+if gender == "1" and Char_class == "1":
+  def char_stand():
+    print("      ",char_name)
+    print("       (▐▌)")
+    print("       ╘╡╞╛")
+    print("       _/\_")
+if gender == 1 and Char_class == 2:
+  def char_stand():
+    print("      ",char_name)
+    print("      ( ▌▐ )")
+    print("        \/  ")
+    print("     (W\||/W)")
+    print("       _/\_")
+    print("      ⅄    ⅄")
